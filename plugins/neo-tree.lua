90ef85a9f72d2476e86095864ba42733e4a4eb01
gaokuilin.gkl <gaokuilin.gkl@bytedance.com>
2024-08-18 21:12:15 +0800

    add neo-tree jump cmds

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -15,6 +15,24 @@ return {
             ["os"] = false, 
             ["ot"] = false,
             o = {"open", nowait = true},
+            ["J"] = function(state)
+              local tree = state.tree
+              local node = tree:get_node()
+              local siblings = tree:get_nodes(node:get_parent_id())
+              local renderer = require('neo-tree.ui.renderer')
+              renderer.focus_node(state, siblings[#siblings]:get_id())
+            end,
+            ["K"] = function(state)
+              local tree = state.tree
+              local node = tree:get_node()
+              local siblings = tree:get_nodes(node:get_parent_id())
+              local renderer = require('neo-tree.ui.renderer')
+              renderer.focus_node(state, siblings[1]:get_id())
+            end,
+            ["P"] = function(state)
+              local node = state.tree:get_node()
+              require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+            end
           }
         }
       },
